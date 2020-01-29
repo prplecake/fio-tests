8bcac8a54857e8ee7d98f9145c7f848139219194
Matthew Jorgensen <matthew@jrgnsn.net>
2020-01-29 09:24:34 -0600

    Fixed wrapper

--- run_job.sh
+++ run_job.sh
@@ -5,8 +5,8 @@ _JOB=$(echo $1 | cut -f 1 -d '.')
 _FILE="${_NOW}_$(hostname)_${_JOB}.log"
 if [ -e $_FILE ]; then
     i=0
-    while [[ -e ${_FILE}.${i} ]]; do
-        let i++
+    while [ -e ${_FILE}.${i} ]; do
+        i=$(( i+1 ))
     done
     _FILE=${_FILE}.${i}
 fi
@@ -14,4 +14,5 @@ fi
 # Run fio test but write output to log right away
 fio $1 | tee $_FILE
 
+echo
 echo "Log saved to ${_FILE}"
