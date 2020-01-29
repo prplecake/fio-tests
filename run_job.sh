b6ad7dddb686f04b86289fe7a562b8fcbd8d2e26
Matthew Jorgensen <matthew@jrgnsn.net>
2020-01-29 09:15:08 -0600

    Test for existing file, rename if exists

--- run_job.sh
+++ run_job.sh
@@ -3,10 +3,15 @@
 _NOW=$(date +"%Y%m%d")
 _JOB=$(echo $1 | cut -f 1 -d '.')
 _FILE="${_NOW}_$(hostname)_${_JOB}.log"
+if [ -e $_FILE ]; then
+    i=0
+    while [[ -e ${_FILE}.${i} ]]; do
+        let i++
+    done
+    _FILE=${_FILE}.${i}
+fi
 
 # Run fio test but write output to log right away
-fio $1 > $_FILE
+fio $1 | tee $_FILE
 
-# Test is done, show results.
-cat $_FILE
 echo "Log saved to ${_FILE}"
